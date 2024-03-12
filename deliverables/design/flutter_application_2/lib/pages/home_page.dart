8e8ae29ace0edbd09a9bd3941f4b137fdf49ea37
Hana Tollossa <tollossa.2@wright.edu>
2024-03-11 21:37:14 -0400

    flutter home page added

---
+++ deliverables/design/flutter_application_2/lib/pages/home_page.dart
@@ -0,0 +1,170 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actionsIconTheme: const IconThemeData(
+    size: 30.0,
+    color: Colors.black,
+    opacity: 10.0
+  ),
+        title: const Text('CommenCents',
+            style:
+                TextStyle(fontSize: 45, color: Color.fromRGBO(34, 203, 67, 1))),
+        titleSpacing: 00.0,
+        centerTitle: true,
+        toolbarHeight: 60.2,
+        toolbarOpacity: 0.8,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(25),
+              bottomLeft: Radius.circular(25)),
+        ),
+        elevation: 0.00,
+        backgroundColor: const Color.fromARGB(245, 222, 221, 227),
+
+          leading: GestureDetector(
+      onTap: () { /* Write listener code here */ },
+      child:const  Icon(
+        Icons.home,
+        color: Color.fromRGBO(34, 203, 67, 1)  // add custom icons also
+      ),
+  ),
+  
+  actions: <Widget>[
+    Padding(
+      padding: const EdgeInsets.only(right: 20.0),
+      child: GestureDetector(
+        onTap: () {},
+        child: const Icon(
+          Icons.perm_identity_rounded,
+          size: 26.0,
+          color: Color.fromRGBO(34, 203, 67, 1),
+        ),
+        
+      )
+    ),
+    
+  ],
+      ),
+      backgroundColor: const Color.fromARGB(245, 234, 236, 238),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Color.fromRGBO(161, 139, 175, 1),
+                    shape: BoxShape.rectangle,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 25),
+          //summery
+          //my manu
+
+          Container(
+            width: 300,
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 231, 230, 216),
+                borderRadius: BorderRadius.circular(16)),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(height: 100),
+                Text(
+                  'Income/Expense',
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Color.fromARGB(255, 34, 203, 67),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: 300,
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 231, 230, 216),
+                borderRadius: BorderRadius.circular(16)),
+            child: const Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 100),
+                Text(
+                  'Budget',
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Color.fromARGB(255, 34, 203, 67),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: 300,
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                color: Color.fromARGB(255, 231, 230, 216),
+                borderRadius: BorderRadius.circular(16)),
+            child: const Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 100),
+                Text(
+                  'Goal',
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Color.fromARGB(255, 34, 203, 67),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      )),
+      bottomNavigationBar: const GNav(
+          activeColor: Color.fromARGB(255, 94, 96, 175),
+          color: Color.fromARGB(255, 34, 203, 67),
+          tabs: [
+            GButton(
+              icon: Icons.add_circle,
+              text: 'Add',
+            ),
+            GButton(
+              icon: Icons.money,
+              text: 'Budget',
+            ),
+            GButton(
+              icon: Icons.align_vertical_bottom,
+              text: 'Metrics', //income/expence
+            ),
+            GButton(
+              icon: Icons.savings,
+              text: 'Goals',
+            ),
+          ]),
+    );
+  }
+}
